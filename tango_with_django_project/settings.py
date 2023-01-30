07cd7c8cf4307bde865005287ac4539b1721dae9
Simone <2827442l@student.gla.ac.uk>
2023-01-30 20:58:53 +0000

    Chapter5 Successful

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
