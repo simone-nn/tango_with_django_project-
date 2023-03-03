04d9fd7b18a04bc7e36b7f0c0fd6d0bb4c06cb83
niidle <123129491+shoutingniidle@users.noreply.github.com>
2023-03-03 11:18:07 +0000

    Delete rmr directory

--- rmr/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RmrConfig(AppConfig):
-    name = 'rmr'
--- rmr/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rmr import views
-
-app_name = 'rmr'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
